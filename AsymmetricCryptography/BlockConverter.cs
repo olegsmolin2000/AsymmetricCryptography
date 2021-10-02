ee079f73f5e3fd7391bb957e6ce9fcd672ff224f
Oleg Smolin <olegsmolin2000@mail.ru>
2021-10-02 19:12:03 +0300

    add BlockConverter class

---
+++ AsymmetricCryptography/BlockConverter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace AsymmetricCryptography
+{
+    static class BlockConverter
+    {
+        //функция нахождени количества байтов, которые можно обрабатывать по заданному модулю
+        public static int GetBlockSize(BigInteger modulus)
+        {
+            const int BYTE_ELEMENTS_COUNT = 256;
+
+            BigInteger byteSize = BYTE_ELEMENTS_COUNT;
+            
+            int size = 1;
+
+            while (byteSize * BYTE_ELEMENTS_COUNT < modulus)
+            {
+                byteSize *= BYTE_ELEMENTS_COUNT;
+                size++;
+            }
+
+            return size;
+        }
+
+        //функция для получения BigInt блоков из произвольного количества байтов
+        public static BigInteger[] BytesToBlocks(byte[] message,int blockSize)
+        {
+            //нахождение количества будущих блоков
+            int blocksCount = message.Length / blockSize;
+
+            if (message.Length % blockSize != 0)
+                blocksCount++;
+
+            BigInteger[] blocks = new BigInteger[blocksCount];
+            
+            //внешний цикл даёт 1 блок на каждой итерации
+            for (int i = 0; i < message.Length ; i+=blockSize)
+            {
+                StringBuilder binaryBlock = new StringBuilder();
+
+                //внутренний цикл записывает вместе байты в двоичном виде
+                for (int j = i; j < i + blockSize && j < message.Length; j++)
+                {
+                    binaryBlock.Append(Convert.ToString(message[j], 2).PadLeft(8, '0'));
+                }
+
+                //перевод блока из двоичного вида в десятичный
+                blocks[i / blockSize] = BinaryConvertings.BinaryToBigInt(binaryBlock.ToString());
+            }
+
+            return blocks;
+        }
+    }
+}
--- AsymmetricCryptography/Program.cs
+++ AsymmetricCryptography/Program.cs
@@ -9,7 +9,36 @@ namespace AsymmetricCryptography
     {
         static void Main(string[] args)
         {
-            
+            Random rand = new Random();
+
+            int size = rand.Next(50);
+            int blockSize = rand.Next(1,10);
+
+            size = 9;
+            blockSize = 8;
+
+            Console.WriteLine("size:" + size);
+            Console.WriteLine("blockSize:" + blockSize);
+
+            Console.WriteLine("----------------------------");
+
+            byte[] bytes = new byte[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                bytes[i] = Convert.ToByte(rand.Next() % 256);
+
+                Console.WriteLine(Convert.ToString(bytes[i], 2).PadLeft(8, '0'));
+            }
+
+            BigInteger[] blocks = BlockConverter.BytesToBlocks(bytes, blockSize);
+
+            Console.WriteLine("----------------------------");
+            for (int i = 0; i < blocks.Length; i++)
+            {
+                Console.WriteLine(BinaryConvertings.BigIntToBinary(blocks[i]).PadRight(BinaryConvertings.GetBinaryLength(blocks[i]),'0'));
+            }
+
         }
     }
 }
