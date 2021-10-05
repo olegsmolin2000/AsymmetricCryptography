d7e0415ad6ce68f1fb47993ab41fa907cc803c79
Oleg Smolin <olegsmolin2000@mail.ru>
2021-10-05 22:23:03 +0300

    add rsa algorithm
    feat: rsa encryption, decryption in RsaAlgorithm class

--- AsymmetricCryptography/Program.cs
+++ AsymmetricCryptography/Program.cs
@@ -10,11 +10,47 @@ namespace AsymmetricCryptography
     {
         static void Main(string[] args)
         {
-            RsaPrivateKey q1;
-            RsaPublicKey q2;
-            KeysGenerator.RsaKeysGeneration(512, out q1, out q2);
-            q1.PrintConsole();
-            q2.PrintConsole();
+            Random rand = new Random();
+            int s = 0;
+            int count = 100000;
+            for (int i = 0; i < count; i++)
+            {
+                //Console.WriteLine(new string('-', 25));
+                if (i % 100 == 0)
+                    Console.WriteLine(i);
+                int size = rand.Next() % 50;
+
+                StringBuilder message = new StringBuilder();
+
+                for (int j = 0; j < size; j++)
+                {
+                    message.Append(Convert.ToChar(rand.Next() % 255+ 1));
+                }
+
+                string mes = message.ToString();
+                //mes = "zhopa o4ko mocha huy ebalo";
+                RsaPrivateKey q1;
+                RsaPublicKey q2;
+                KeysGenerator.RsaKeysGeneration(32, out q1, out q2);
+                byte[] crypt = RsaAlgorithm.Encrypt(Encoding.UTF8.GetBytes(mes), q2);
+                string decrypt = Encoding.Default.GetString(RsaAlgorithm.Decryption(crypt, q1));
+                
+                if (mes == decrypt)
+                    s++;
+                else
+                {
+                    Console.WriteLine("mes:" + mes);
+                    //Console.WriteLine(mes.Length);
+                    Console.WriteLine("decrypt:" + decrypt);
+                    //Console.WriteLine(decrypt.Length);
+                    //q1.PrintConsole();
+                    //q2.PrintConsole();
+                }
+                //Console.WriteLine(new string('-', 25));
+            }
+            Console.WriteLine("s:" + s);
+            Console.WriteLine("count:" + count);
+            Console.WriteLine("error:" + (1-(float)s/(float)count)*100+"%");
         }
     }
 }
---
+++ AsymmetricCryptography/RSA/RsaAlgorithm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace AsymmetricCryptography.RSA
+{
+    static class RsaAlgorithm
+    {
+        //шифрование RSA с помощью открытого ключа
+        public static byte[] Encrypt(byte[] data,RsaPublicKey publicKey)
+        {
+            //получение отклытой экпоненты и модуля
+            BigInteger exponent = publicKey.Exponent;
+            BigInteger modulus = publicKey.Modulus;
+
+            //вычисление размера блока
+            int blockSize = BlockConverter.GetBlockSize(modulus);
+
+            //перевод массива байтов в блоки BigInt
+            BigInteger[] blocks = BlockConverter.BytesToBlocks(data, blockSize);
+
+            //в список будут заноситься байты, полученные из зашифрованных блоков
+            List<byte> encryptedBytes = new List<byte>();
+
+            //каждый блок возводится в степень експоненты и берётся по модулю. переводится обратно в байты и запоминается
+            for (int i = 0; i < blocks.Length; i++)
+            {
+                blocks[i] = BigInteger.ModPow(blocks[i], exponent, modulus);
+
+                encryptedBytes.AddRange(BlockConverter.BlockToBytes(blocks[i],blockSize+1));
+            }
+
+            //в результате получается массив байтов зашифрованных данных
+            return encryptedBytes.ToArray();
+        }
+
+        //расшифровка RSA
+        public static byte[] Decryption(byte[] encryptedData,RsaPrivateKey privateKey)
+        {
+            //получение закрытой экспоненты и модуля
+            BigInteger exponent = privateKey.PrivateExponent;
+            BigInteger modulus = privateKey.Modulus;
+
+            //вычисление размера блоков
+            int blockSize = BlockConverter.GetBlockSize(modulus);
+
+            //перевод зашифрованных данных в блоки BigInt
+            BigInteger[] blocks = BlockConverter.BytesToBlocks(encryptedData, blockSize+1);
+
+            //в список будет заноситься результат дешифровки
+            List<byte> decryptedBytes = new List<byte>();
+
+            //каждый блок возводится в степень закрытой экспоненты и берётся по модулю
+            for (int i = 0; i < blocks.Length; i++)
+            {
+                blocks[i] = BigInteger.ModPow(blocks[i], exponent, modulus);
+
+                //в список добавляются расшифрованные байты
+                decryptedBytes.AddRange(BlockConverter.BlockToBytes(blocks[i], blockSize + 1));
+            }
+
+            decryptedBytes.RemoveAll(x => x == 0);
+
+            return decryptedBytes.ToArray();
+        }
+    }
+}
