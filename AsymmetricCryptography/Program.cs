751811c639fa54b0350bd7cbea1f394247c38449
Oleg Smolin <olegsmolin2000@mail.ru>
2021-10-10 00:25:07 +0300

    add md-5 crypto hash algorithm

--- AsymmetricCryptography/Program.cs
+++ AsymmetricCryptography/Program.cs
@@ -13,76 +13,99 @@ namespace AsymmetricCryptography
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
+            //    Random rand = new Random();
 
-            int s = 0;
-            int count = 1000;
+            //    int s = 0;
+            //    int count = 1000;
 
-            for (int i = 0; i < count; i++)
-            {
-                //if (i % 10 == 0)
-                    Console.WriteLine(i);
-                DsaPrivateKey q1;
-                DsaPublicKey q2;
-                RsaPrivateKey q11;
-                RsaPublicKey q22;
-                KeysGenerator.RsaKeysGeneration(24, out q11, out q22);
-                KeysGenerator.DsaKeysGeneration(48, 32, out q1, out q2);
-                //System.Security.Cryptography.RSACryptoServiceProvider
-                StringBuilder message = new StringBuilder();
-                AsymmetricKey qq = q11;
-                int mesLength = rand.Next() % 50;
+            //    for (int i = 0; i < count; i++)
+            //    {
+            //        //if (i % 10 == 0)
+            //            Console.WriteLine(i);
+            //        DsaPrivateKey q1;
+            //        DsaPublicKey q2;
+            //        RsaPrivateKey q11;
+            //        RsaPublicKey q22;
+            //        KeysGenerator.RsaKeysGeneration(24, out q11, out q22);
+            //        KeysGenerator.DsaKeysGeneration(48, 32, out q1, out q2);
+            //        //System.Security.Cryptography.RSACryptoServiceProvider
+            //        StringBuilder message = new StringBuilder();
+            //        AsymmetricKey qq = q11;
+            //        int mesLength = rand.Next() % 50;
 
-                ElGamalPrivateKey qq1;
-                ElGamalPublicKey qq2;
+            //        ElGamalPrivateKey qq1;
+            //        ElGamalPublicKey qq2;
 
-                KeysGenerator.ElGamalKeysGeneration(32, out qq1, out qq2);
+            //        KeysGenerator.ElGamalKeysGeneration(32, out qq1, out qq2);
 
-                for (int j = 0; j < mesLength; j++)
-                {
-                    message.Append(Convert.ToChar(Convert.ToByte(rand.Next() % 255+1)));
-                }
-                
-                RsaAlgorithm rsa1 = new RsaAlgorithm(q11,q22);
-                
-                DSA dsa1 = new DSA(q1, q2);
+            //        for (int j = 0; j < mesLength; j++)
+            //        {
+            //            message.Append(Convert.ToChar(Convert.ToByte(rand.Next() % 255+1)));
+            //        }
 
-                ElGamalAlgorithm elg = new ElGamalAlgorithm(qq1,qq2);
+            //        RsaAlgorithm rsa1 = new RsaAlgorithm(q11,q22);
 
-                byte[] data = Encoding.UTF8.GetBytes(message.ToString());
+            //        DSA dsa1 = new DSA(q1, q2);
 
-                DigitalSignature sign = dsa1.CreateSignature(data, new SHA_256());
-                DigitalSignature rsaSign = rsa1.CreateSignature(data, new SHA_256());
-                DigitalSignature elgamalds = elg.CreateSignature(data, new SHA_256());
+            //        ElGamalAlgorithm elg = new ElGamalAlgorithm(qq1,qq2);
 
-                bool dsa = dsa1.VerifyDigitalSignature(sign, data, new SHA_256());
-                bool rsa = rsa1.VerifyDigitalSignature(rsaSign, data, new SHA_256());
-                bool elgamal = elg.VerifyDigitalSignature(elgamalds, data, new SHA_256());
-                
+            //        byte[] data = Encoding.UTF8.GetBytes(message.ToString());
 
-                var crypt = rsa1.Encrypt(data);
-                var decryp = rsa1.Decrypt(crypt);
+            //        DigitalSignature sign = dsa1.CreateSignature(data, new SHA_256());
+            //        DigitalSignature rsaSign = rsa1.CreateSignature(data, new SHA_256());
+            //        DigitalSignature elgamalds = elg.CreateSignature(data, new SHA_256());
 
-                var egcrypt = elg.Encrypt(data);
-                var egdecrypt = elg.Decrypt(egcrypt);
+            //        bool dsa = dsa1.VerifyDigitalSignature(sign, data, new SHA_256());
+            //        bool rsa = rsa1.VerifyDigitalSignature(rsaSign, data, new SHA_256());
+            //        bool elgamal = elg.VerifyDigitalSignature(elgamalds, data, new SHA_256());
 
 
-                if (!dsa)
-                    Console.WriteLine("dsa error");
-                if (!rsa)
-                    Console.WriteLine("rsa sign error");
-                if (Encoding.UTF8.GetString(decryp) != message.ToString())
-                    Console.WriteLine("rsa crypt error");
-                if (Encoding.UTF8.GetString(egdecrypt) != message.ToString())
-                    Console.WriteLine("el gamal crypt error");
-                if (!elgamal)
-                    Console.WriteLine("el gamal sign error");
+            //        var crypt = rsa1.Encrypt(data);
+            //        var decryp = rsa1.Decrypt(crypt);
 
-            }
+            //        var egcrypt = elg.Encrypt(data);
+            //        var egdecrypt = elg.Decrypt(egcrypt);
 
 
-            Console.WriteLine("s:" + s);
-            Console.WriteLine("count:" + count);
+            //        if (!dsa)
+            //            Console.WriteLine("dsa error");
+            //        if (!rsa)
+            //            Console.WriteLine("rsa sign error");
+            //        if (Encoding.UTF8.GetString(decryp) != message.ToString())
+            //            Console.WriteLine("rsa crypt error");
+            //        if (Encoding.UTF8.GetString(egdecrypt) != message.ToString())
+            //            Console.WriteLine("el gamal crypt error");
+            //        if (!elgamal)
+            //            Console.WriteLine("el gamal sign error");
+
+            //    }
+
+
+            //    Console.WriteLine("s:" + s);
+            //    Console.WriteLine("count:" + count);
+
+
+
+
+
+
+
+
+            //while (true)
+            //{
+            //    string text = Console.ReadLine();
+
+            //    CryptographicHashAlgorithm hashAlgorithm = new MD_5();
+
+            //    var hash = hashAlgorithm.GetHash(Encoding.UTF8.GetBytes(text));
+
+            //    for (int i = 0; i < hash.Length; i++)
+            //    {
+            //        Console.Write(hash[i].ToString("x"));
+            //    }
+            //    Console.WriteLine();
+            //}
+
         }
     }
 }
