90f6f1885c63bc9cf81af4e5ab8eec85bb0b0c8c
Oleg Smolin <olegsmolin2000@mail.ru>
2021-10-28 17:34:29 +0300

    start making new DAL with ef core
    create abstract key class

---
+++ AsymmetrycCryptographyDataLayer/Entities/Keys/AsymmetricKey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsymmetrycCryptographyDataLayer.Entities.Keys
+{
+    public class AsymmetricKey
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AlgorithmName { get; set; }
+        public string Permittion { get; set; }
+
+        //public List<RsaPrivateKey> RsaPrivateKeys { get; set; }
+        //public List<RsaPublicKey> RsaPublicKeys { get; set; }
+    }
+}
---
+++ AsymmetrycCryptographyDataLayer/Entities/Keys/RsaPublicKey.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel.DataAnnotations.Schema;
+//using System.Linq;
+//using System.Numerics;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace AsymmetrycCryptographyDataLayer.Entities.Keys
+//{
+//    public class RsaPublicKey : AsymmetricKey
+//    {
+//        public string test2 { get; set; }
+//        //public BigInteger Exponent { get; }//e
+//        //public BigInteger Modulus { get; }//n
+//    }
+//}
