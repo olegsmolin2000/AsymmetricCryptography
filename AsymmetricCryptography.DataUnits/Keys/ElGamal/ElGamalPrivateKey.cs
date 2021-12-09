72dcaae05a862da82db8dcccdae34a02d5395ad3
Oleg Smolin <olegsmolin2000@mail.ru>
2021-12-09 20:29:30 +0300

    create elgamal keys

---
+++ AsymmetricCryptography.DataUnits/Keys/ElGamal/ElGamalPrivateKey.cs
@@ -0,0 +1,21 @@
+﻿namespace AsymmetricCryptography.DataUnits.Keys.ElGamal
+{
+    public sealed class ElGamalPrivateKey : AsymmetricKey
+    {
+        public BigInteger P { get; init; }
+        public BigInteger G { get; init; }
+
+        public BigInteger X { get; init; }
+
+        private ElGamalPrivateKey(int binarySize) 
+            : base(AlgorithmName.ElGamal, KeyType.Private, binarySize) { }
+
+        public ElGamalPrivateKey(int binarySize, BigInteger p, BigInteger g, BigInteger x)
+            :this(binarySize)
+        {
+            P = p;
+            G = g;
+            X = x;
+        }
+    }
+}
---
+++ AsymmetricCryptography.DataUnits/Keys/ElGamal/ElGamalPublicKey.cs
@@ -0,0 +1,21 @@
+﻿namespace AsymmetricCryptography.DataUnits.Keys.ElGamal
+{
+    public sealed class ElGamalPublicKey : AsymmetricKey
+    {
+        public BigInteger P { get; init; }
+        public BigInteger G { get; init; }
+
+        public BigInteger Y { get; init; }
+
+        private ElGamalPublicKey(int binarySize) 
+            : base(AlgorithmName.ElGamal, KeyType.Public, binarySize) { }
+
+        public ElGamalPublicKey(int binarySize, BigInteger p, BigInteger g, BigInteger y)
+            : this(binarySize)
+        {
+            P = p;
+            G = g;
+            Y = y;
+        }
+    }
+}
