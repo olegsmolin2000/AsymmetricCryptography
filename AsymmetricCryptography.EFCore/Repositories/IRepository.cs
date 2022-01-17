176920b9727e39b6e9be9e151ad1afe52f579cfa
Oleg Smolin <olegsmolin2000@mail.ru>
2022-01-17 23:42:18 +0300

    add irepository and derive keys repository

---
+++ AsymmetricCryptography.EFCore/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace AsymmetricCryptography.EFCore.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> Items { get; }
+
+        T Get(int id);
+
+        void Add(T item);
+    }
+}
---
+++ AsymmetricCryptography.EFCore/Repositories/KeysRepository.cs
@@ -0,0 +1,42 @@
+﻿using AsymmetricCryptography.EFCore.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsymmetricCryptography.EFCore.Repositories
+{
+    public class KeysRepository<T> : IRepository<T> where T : AsymmetricKey
+    {
+        private readonly KeysContext Db;
+        private readonly DbSet<T> Set;
+
+        public KeysRepository(KeysContext db)
+        {
+            Db = db;
+            Set = db.Set<T>();
+        }
+
+        public IEnumerable<T> Items => Set.ToList();
+
+        public void Add(T item)
+        {
+            if (item is null)
+                throw new ArgumentNullException(nameof(item));
+
+            if (Set.Contains(item))
+                throw new ArgumentException("DB contains this key");
+
+            Set.Add(item);
+
+            Db.SaveChanges();
+        }
+
+        public T Get(int id)
+        {
+            var key = Items.FirstOrDefault(item => item.Id == id);
+
+            if (key is null)
+                throw new ArgumentException("Incorrect id");
+            else
+                return key;
+        }
+    }
+}
